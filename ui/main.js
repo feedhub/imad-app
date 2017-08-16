b896505112598abc3040a1a87e7c915a59aeb1db
feedhub <khadijab37@gmail.com>
2017-08-16 17:55:52 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,6 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'I\'m Khadija';
-
 //move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
