2a825eac8e821a1a48f44093bc419e3edd8bfc5d
feedhub <khadijab37@gmail.com>
2017-08-16 18:20:56 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,5 +5,5 @@ element.innerHTML = 'I\'m Khadija';
 //move the image
 var img = document.getElementById('madi');
 img.onclick = function () {
-    img.style.margin-left = '1000px';
+    img.style.margintop = '100px';
 };
