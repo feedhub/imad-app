1cd0334bc7912a046d0aeecd1c60a6969039b019
feedhub <khadijab37@gmail.com>
2017-08-22 09:53:32 +0300

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,5 +33,12 @@ submit.onclick = function(){
   //make a request to server and send the name
   
   //capture the name and render it into a list
-    
+    var names = ['name1','name2','name3'];
+    var list = '';
+    for(var i=0; i<names.length; i++){
+        list <= '<li>' + names[i] + '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
 };
