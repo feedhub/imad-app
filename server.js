ba8897bcde72620e48e51e1ba3b41f6390e1657c
feedhub <khadijab37@gmail.com>
2017-08-26 17:30:51 +0300

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,11 +108,22 @@ function createTemplate(data) {
     return htmlTemplate;
 }
 
-app.get('/:articleName', function (req, res) {
-  var articleName = req.params.articleName;
+app.get('/article/:articleName', function (req, res) {
   //artcleName == article-one
   //articles[articleName] == {} content object for article-one
-  res.send(createTemplate(articles[articleName]));
+  pool.query("SELECT * from article where title=" + req.params.articleName, function(err,result){
+      if (err){
+          res.status(500).send(err.toString());
+      }else {
+          if(result.rows.length === 0){
+              res.status(404).send('Article not found');
+          }else{
+              var articleData = result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+  });
+  
 });
 
 app.get('/ui/style.css', function (req, res) {
